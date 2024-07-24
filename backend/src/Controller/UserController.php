ba776e84aec5dc9f8ab536aa9dc54f7f6f6fe52a
Amilcar-afk <fernandesamilcar28@gmail.com>
2024-07-24 16:08:23 +0200

    controllers for user ok

--- backend/src/Controller/UserController.php
+++ backend/src/Controller/UserController.php
@@ -7,75 +7,120 @@ use App\Form\UserType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 
 #[Route('/user')]
 class UserController extends AbstractController
 {
-    #[Route('/', name: 'app_user_index', methods: ['GET'])]
-    public function index(UserRepository $userRepository): Response
+
+    private CsrfTokenManagerInterface $csrfTokenManager;
+
+    public function __construct(CsrfTokenManagerInterface $csrfTokenManager)
     {
-        return $this->render('user/index.html.twig', [
-            'users' => $userRepository->findAll(),
-        ]);
+        $this->csrfTokenManager = $csrfTokenManager;
     }
 
-    #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/', name: 'user_index', methods: ['GET'])]
+    public function index(UserRepository $userRepository, SerializerInterface $serializer): Response
     {
+        $users = $userRepository->findAll();
+        $json = $serializer->serialize($users, 'json');
+
+        return new Response($json, 200, ['Content-Type' => 'application/json']);
+    }
+
+    #[Route('/new', name: 'user_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer): Response
+    {
+        if ($request->isMethod('GET')) {
+            $user = new User();
+            $form = $this->createForm(UserType::class, $user);
+
+            return $this->render('user/new.html.twig', [
+                'user' => $user,
+                'form' => $form->createView(),
+            ]);
+        }
+
+        $data = json_decode($request->getContent(), true);
         $user = new User();
+
         $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
+        $form->submit($data);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->persist($user);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+            $json = $serializer->serialize($user, 'json');
+
+            return new Response($json, 201, ['Content-Type' => 'application/json']);
         }
 
-        return $this->render('user/new.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
+        return new Response(null, 400);
     }
 
-    #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
-    public function show(User $user): Response
+    #[Route('/{id}', name: 'user_show', methods: ['GET'])]
+    public function show(User $user, SerializerInterface $serializer): Response
     {
-        return $this->render('user/show.html.twig', [
-            'user' => $user,
-        ]);
+        $json = $serializer->serialize($user, 'json');
+
+        return new Response($json, 200, ['Content-Type' => 'application/json']);
     }
 
-    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    #[Route('/{id}/edit', name: 'user_edit', methods: ['PUT'])]
+    public function edit(Request $request, User $user, EntityManagerInterface $entityManager, SerializerInterface $serializer): Response
     {
+        $data = json_decode($request->getContent(), true);
         $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
+        $form->submit($data);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+            $json = $serializer->serialize($user, 'json');
+
+            return new Response($json, 200, ['Content-Type' => 'application/json']);
         }
 
-        return $this->render('user/edit.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
+        return new Response(null, 400);
     }
 
-    #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'user_delete', methods: ['DELETE'])]
     public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($user);
-            $entityManager->flush();
+        $data = json_decode($request->getContent(), true);
+
+        if (!$data) {
+            return new Response('Invalid JSON data', 400);
         }
 
-        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        if (!isset($data['_token'])) {
+            return new Response('CSRF token missing', 400);
+        }
+
+        $isValidToken = $this->isCsrfTokenValid('delete_user' . $user->getId(), $data['_token']);
+        if (!$isValidToken) {
+            return new Response('Invalid CSRF token', 400);
+        }
+
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        return new Response(null, 204);
+    }
+
+    #[Route('/{id}/csrf-token', name: 'user_csrf_token', methods: ['GET'])]
+    public function getCsrfTokenForUser(int $id): JsonResponse
+    {
+        // Générer un token CSRF pour l'action de suppression d'un utilisateur
+        $token = $this->csrfTokenManager->getToken('delete_user' . $id)->getValue();
+
+        return new JsonResponse(['csrfToken' => $token]);
     }
 }
