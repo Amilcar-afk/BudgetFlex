085d1611c99f5b97a2053e8f9d609abc4914752f
Amilcar <fernandesamilcar28@gmail.com>
2025-01-15 14:22:43 +0100

    remove category from entity

---
+++ backend/migrations/Version20250115132209.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250115132209 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE budget_month DROP needs_category');
+        $this->addSql('ALTER TABLE budget_month DROP saving_category');
+        $this->addSql('ALTER TABLE budget_month DROP wants_category');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE budget_month ADD needs_category DOUBLE PRECISION DEFAULT \'0\' NOT NULL');
+        $this->addSql('ALTER TABLE budget_month ADD saving_category DOUBLE PRECISION DEFAULT \'0\' NOT NULL');
+        $this->addSql('ALTER TABLE budget_month ADD wants_category DOUBLE PRECISION DEFAULT \'0\' NOT NULL');
+    }
+}
